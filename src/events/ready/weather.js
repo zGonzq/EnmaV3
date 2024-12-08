ef7adaf83f4f77dd5a1e628755f5cdd6b9de5969
zGonzq <gonzaloandres2508@gmail.com>
2024-12-08 16:05:10 -0300

    clima mayusc

--- src/events/ready/weather.js
+++ src/events/ready/weather.js
@@ -16,21 +16,23 @@ module.exports = async (client) => {
         const weatherResponse = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=Pitrufquen&appid=${process.env.WEATHER_API_KEY}&units=metric&lang=es`);
         const weather = weatherResponse.data;
     
+        const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+
         const embed = new EmbedBuilder()
             .setTitle('Meteorología')
             .setDescription(`Clima en del servidor en la zona sur de Chile.`)
             .setThumbnail(`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`)
             .addFields(
-                { name: 'Hora', value: formattedTime, inline: true},
-                { name: `Puesta de sol`, value: `<t:${weather.sys.sunset}:T>`, inline: true },
-                { name: `Amanecer`, value: `<t:${weather.sys.sunrise}:T>`, inline: true },
-                { name: 'Temperatura', value: `${weather.main.temp}°C`, inline: false },
-                { name: 'Sensación térmica', value: `${weather.main.feels_like}°C`, inline: false },
-                { name: `Humedad`, value: `${weather.main.humidity}%`, inline: false },
-                { name: `Presión`, value: `${weather.main.pressure} hPa`, inline: false },
-                { name: `Estado del cielo`, value: `${weather.weather[0].description}`, inline: false },
-                { name: `Viento`, value: `${weather.wind.speed} m/s`, inline: false },
-                { name: `Nubosidad`, value: `${weather.clouds.all}%`, inline: false },
+            { name: 'Hora', value: formattedTime, inline: true},
+            { name: `Puesta de sol`, value: `<t:${weather.sys.sunset}:T>`, inline: true },
+            { name: `Amanecer`, value: `<t:${weather.sys.sunrise}:T>`, inline: true },
+            { name: 'Temperatura', value: `${weather.main.temp}°C`, inline: false },
+            { name: 'Sensación térmica', value: `${weather.main.feels_like}°C`, inline: false },
+            { name: `Humedad`, value: `${weather.main.humidity}%`, inline: false },
+            { name: `Presión`, value: `${weather.main.pressure} hPa`, inline: false },
+            { name: `Estado del cielo`, value: `${capitalizeFirstLetter(weather.weather[0].description)}`, inline: false },
+            { name: `Viento`, value: `${weather.wind.speed} m/s`, inline: false },
+            { name: `Nubosidad`, value: `${weather.clouds.all}%`, inline: false },
             )
             .setColor('Random');
     
