39187e7f19973c153759225553622515adc239ed
zGonzq <gonzaloandres2508@gmail.com>
2024-12-10 00:13:35 -0300

    asd

--- src/commands/integrations/findpokemon.js
+++ src/commands/integrations/findpokemon.js
@@ -28,6 +28,7 @@ module.exports = {
 
       embed.setTitle(pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1))
         .setThumbnail(pokemon.sprites.front_default)
+        .setDescription(species.genera[7].genus)
         .addFields(
           { name: 'ID', value: pokemon.id.toString(), inline: true },
           { name: 'Altura', value: `${pokemon.height / 10} m`, inline: true },
@@ -38,7 +39,6 @@ module.exports = {
           { name: 'Hábitat', value: species.habitat ? species.habitat.name : 'Desconocido', inline: true },
           { name: 'Color', value: species.color.name, inline: true },
           { name: 'Forma', value: species.shape.name, inline: true },
-          { name: 'Descripción', value: species.flavor_text_entries.find(entry => entry.language.name === 'es').flavor_text, inline: false }
         )
         .setColor('Random');
 
