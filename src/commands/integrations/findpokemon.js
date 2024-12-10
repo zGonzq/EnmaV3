ed6fb52acd3091a1e19f3cf4e06278cdaf927bee
zGonzq <gonzaloandres2508@gmail.com>
2024-12-10 00:16:46 -0300

    asd

--- src/commands/integrations/findpokemon.js
+++ src/commands/integrations/findpokemon.js
@@ -26,21 +26,19 @@ module.exports = {
       const speciesResponse = await axios.get(pokemon.species.url);
       const species = speciesResponse.data;
 
-      embed.setTitle(pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1))
-        .setThumbnail(pokemon.sprites.front_default)
-        .setDescription(species.genera[7].genus)
-        .addFields(
-          { name: 'ID', value: pokemon.id.toString(), inline: true },
-          { name: 'Altura', value: `${pokemon.height / 10} m`, inline: true },
-          { name: 'Peso', value: `${pokemon.weight / 10} kg`, inline: true },
-          { name: 'Tipos', value: pokemon.types.map(type => type.type.name).join(', '), inline: true },
-          { name: 'Habilidades', value: pokemon.abilities.map(ability => ability.ability.name).join(', '), inline: true },
-          { name: 'Movimientos', value: pokemon.moves.slice(0, 5).map(move => move.move.name).join(', '), inline: false },
-          { name: 'Hábitat', value: species.habitat ? species.habitat.name : 'Desconocido', inline: true },
-          { name: 'Color', value: species.color.name, inline: true },
-          { name: 'Forma', value: species.shape.name, inline: true },
-        )
-        .setColor('Random');
+    embed.setTitle(pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1))
+      .setThumbnail(pokemon.sprites.front_default)
+      .setDescription(species.flavor_text_entries.find(entry => entry.language.name === 'es').flavor_text)
+      .addFields(
+        { name: 'ID', value: pokemon.id.toString(), inline: true },
+        { name: 'Altura', value: `${pokemon.height / 10} m`, inline: true },
+        { name: 'Peso', value: `${pokemon.weight / 10} kg`, inline: true },
+        { name: 'Tipos', value: pokemon.types.map(type => type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1)).join(', '), inline: true },
+        { name: 'Hábitat', value: species.habitat ? species.habitat.name.charAt(0).toUpperCase() + species.habitat.name.slice(1) : 'Desconocido', inline: true },
+        { name: 'Color', value: species.color.name.charAt(0).toUpperCase() + species.color.name.slice(1), inline: true },
+        { name: 'Forma', value: species.shape.name.charAt(0).toUpperCase() + species.shape.name.slice(1), inline: true },
+      )
+      .setColor('Random');
 
       await interaction.reply({ embeds: [embed] });
 
