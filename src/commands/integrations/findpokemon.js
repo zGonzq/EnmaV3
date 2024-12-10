d63a2a4aa7f8a42e9ce6ac77baa26c507d6390e7
zGonzq <gonzaloandres2508@gmail.com>
2024-12-09 23:58:14 -0300

    INTEGRATIONS

---
+++ src/commands/integrations/findpokemon.js
@@ -0,0 +1,45 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+  /** @type {import('commandkit').CommandData}  */
+  data: new SlashCommandBuilder()
+    .setName('findpokemon')
+    .setDescription('Busca información de un Pokémon específico')
+    .addStringOption(option => 
+      option.setName('pokemon')
+        .setDescription('Nombre o ID del Pokémon')
+        .setRequired(true)
+    ),
+
+  /**
+   * @param {import('commandkit').SlashCommandProps} param0
+   */
+  run: async ({ interaction }) => {
+    const embed = new EmbedBuilder();
+    const pokemonNameOrId = interaction.options.getString('pokemon');
+
+    try {
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonNameOrId.toLowerCase()}`);
+      const pokemon = response.data;
+
+      embed.setTitle(pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1))
+        .setThumbnail(pokemon.sprites.front_default)
+        .addFields(
+          { name: 'ID', value: pokemon.id.toString(), inline: true },
+          { name: 'Altura', value: `${pokemon.height / 10} m`, inline: true },
+          { name: 'Peso', value: `${pokemon.weight / 10} kg`, inline: true },
+          { name: 'Tipos', value: pokemon.types.map(type => type.type.name).join(', '), inline: true },
+          { name: 'Habilidades', value: pokemon.abilities.map(ability => ability.ability.name).join(', '), inline: true },
+          { name: 'Estadísticas', value: pokemon.stats.map(stat => `${stat.stat.name}: ${stat.base_stat}`).join('\n'), inline: false }
+        )
+        .setColor('Random');
+
+      await interaction.reply({ embeds: [embed] });
+
+    } catch (error) {
+      console.log('Error al obtener la información del Pokémon:', error);
+      await interaction.reply({ content: `No se pudo encontrar información sobre el Pokémon **${pokemonNameOrId}**.`, ephemeral: true });
+    }
+  },
+};
