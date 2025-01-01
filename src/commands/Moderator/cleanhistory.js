af60221d85262854931fbf0f43fa4f14efc8b193
zGonzq <gonzaloandres2508@gmail.com>
2025-01-01 18:42:34 -0300

    fixed

--- src/commands/Moderator/cleanhistory.js
+++ src/commands/Moderator/cleanhistory.js
@@ -1,32 +1,26 @@
 const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
+const Moderation = require('../../models/moderation');
 
 module.exports = {
-    /** @type {import('commandkit').CommandData}  */
     data: new SlashCommandBuilder()
         .setName('cleanhistory')
-        .setDescription('Elimina el historial de mensajes de un usuario en el canal actual')
+        .setDescription('Elimina el historial de sanciones de un usuario')
         .addUserOption(option => 
             option.setName('usuario')
-                .setDescription('El usuario cuyo historial de mensajes deseas eliminar')
+                .setDescription('El usuario cuyo historial de sanciones deseas eliminar')
                 .setRequired(true))
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
 
-    /**
-     * @param {import('commandkit').SlashCommandProps} param0
-     */
     run: async ({ interaction }) => {
         const user = interaction.options.getUser('usuario');
         const embed = new EmbedBuilder();
 
         try {
-            const messages = await interaction.channel.messages.fetch({ limit: 100 });
-            const userMessages = messages.filter(msg => msg.author.id === user.id);
-
-            await interaction.channel.bulkDelete(userMessages, true);
-            embed.setDescription(`Se han eliminado ${userMessages.size} mensajes de ${user.tag}.`).setColor('Green');
+            const result = await Moderation.deleteMany({ userId: user.id, guildId: interaction.guild.id });
+            embed.setDescription(`Se ha eliminado el historial de sanciones de ${user.tag}.`).setColor('Green');
         } catch (error) {
-            console.error('Error al eliminar mensajes:', error);
-            embed.setDescription('Hubo un error al intentar eliminar los mensajes.').setColor('Red');
+            console.error('Error al eliminar el historial de sanciones:', error);
+            embed.setDescription('Hubo un error al intentar eliminar el historial de sanciones.').setColor('Red');
         }
 
         return interaction.reply({ embeds: [embed], ephemeral: true });
